e4586170e51e816ac0533c3353860eaf48b3afe2
LeoKrasowski <leokrasowski@gmail.com>
2025-02-06 13:37:47 +0100

    Car class added

---
+++ src/test/java/com/example/car_booking_system/RoadrunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadrunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
